d482e0129f1dd60a20c8717a64c858a8111bcc36
Omar Jawed <jawedsy@sheridancollege.ca>
2020-02-05 14:36:29 -0500

    F

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/play.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ca.sheridancollege.project;
+
+/**
+ * @Omar Jawed
+ *@991593731
+ */
+public class play {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
