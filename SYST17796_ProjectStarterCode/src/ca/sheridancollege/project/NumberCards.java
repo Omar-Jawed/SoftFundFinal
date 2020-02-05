d785b84be9ca719aebf9581ae50408383e73b76e
Omar Jawed <jawedsy@sheridancollege.ca>
2020-02-05 13:45:41 -0500

    Use starter code for assignment

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/NumberCards.java
@@ -0,0 +1,27 @@
+package ca.sheridancollege.project;
+
+public class NumberCards {
+	public enum COLORS {
+		RED, YELLOW, GREEN, BLUE
+	};
+
+	public enum VALUE {
+		ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+	};
+
+	public void numCards() {
+
+	}
+
+	public void skip() {
+
+	}
+
+	public void reverse() {
+
+	}
+
+	public void plus2() {
+
+	}
+}
