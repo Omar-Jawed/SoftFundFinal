ef6eb633e58d67b89356f4bb81d1d798411140d1
Omar Jawed <jawedsy@sheridancollege.ca>
2020-04-12 15:14:42 -0400

    Creating Cards

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/NumberCards.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/NumberCards.java
@@ -1,13 +1,53 @@
 package ca.sheridancollege.project;
 
 public class NumberCards {
-	public enum COLORS {
-		RED, YELLOW, GREEN, BLUE
-	};
+	public enum Color {
+        BLUE   { public String toString() { return "BLUE"; }},
+        RED    { public String toString() { return "RED"; }},
+        GREEN  { public String toString() { return "GREEN"; }},
+        YELLOW { public String toString() { return "YELLOW"; }}
+    }
 
-	public enum VALUE {
-		ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
-	};
+    public enum Value {
+        ZERO    { public String toString() { return "0";}},
+        ONE     { public String toString() { return "1";}},
+        TWO     { public String toString() { return "2";}},
+        THREE   { public String toString() { return "3";}},
+        FOUR    { public String toString() { return "4";}},
+        FIVE    { public String toString() { return "5";}},
+        SIX     { public String toString() { return "6";}},
+        SEVEN   { public String toString() { return "7";}},
+        EIGHT   { public String toString() { return "8";}},
+        NINE    { public String toString() { return "9";}}
+    }
+    
+    public enum Special {
+        SKIP    { public String toString() { return "SKIP";}},
+        REVERSE { public String toString() { return "REVERSE";}},
+        DRAWTWO { public String toString() { return "+2";}}
+    }
+
+    public void EspecialCard(Color color, Special special){
+        this.color = color;
+        this.special = special;
+    }
+
+    private Color color;
+    private Value value;
+    private Special special;
+
+    public void NormalCard(final Color color, final Value value){
+        this.color = color;
+        this.value = value;
+    }
+    
+    public String getColor(){
+    	return this.color.name();
+    }
+    
+    public String getValue(){
+    	return this.value.toString();
+    }
 
 	public void numCards() {
 
@@ -23,5 +63,8 @@ public class NumberCards {
 
 	public void plus2() {
 
+	}
+	public Special getSpecial() {
+		return special;
 	}
 }
